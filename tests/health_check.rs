225f587764e58dff52247307e8bd4cc6e04ee60f
enoent4400 <mihalic1989@gmail.com>
2022-11-15 09:58:53 +0100

    Refactor tests

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,22 +1,33 @@
+use std::net::TcpListener;
+use rust_newsletter::run;
+// Launch our application in the background ~somehow~
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0")
+    .expect("Failed to bind random port");
+    // We retrieve the port assigned to us by the OS
+    let port = listener.local_addr().unwrap().port();
+    let server = run(listener).expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+    // We return the application address to the caller!
+    format!("http://127.0.0.1:{}", port)
+}
+
+
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
-    spawn_app().await.expect("Failed to spawn our app.");
-    // We need to bring in `reqwest`
-    // to perform HTTP requests against our application.
+    let address = spawn_app();
     let client = reqwest::Client::new();
+
     // Act
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        // Use the returned application address
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
+
     // Assert
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
-}
-
-// Launch our application in the background ~somehow~
-async fn spawn_app() -> std::io::Result<()> {
-    rust_newsletter::run().await
 }
